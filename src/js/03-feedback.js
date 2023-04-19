84a70656d4b7c5020d8944b4ce4f29685d10ec80
Evgeniy-D-2022 <116091668+Evgeniy-D-2022@users.noreply.github.com>
2023-04-19 22:25:46 +0200

    update_feedback_03_js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector(".feedback-form");
+const formInput = document.querySelector(".feedback-form input");
+const formMessage = document.querySelector(".feedback-form message");
+const formData = {};
+
+
+const LOCALSTORAGE_KEY = "feedback-form-state";
+
+form.addEventListener('input', throttle(onInput, 500));
+form.addEventListener('submit', onBtnSubmit);
+
+checkStorageContent();
+
+function onInput(event) {
+    formData[event.target.name] = event.target.value
+	localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData))
+};
+
+function checkStorageContent() {
+    const savedFeetbackForm = localStorage.getItem(LOCALSTORAGE_KEY);
+    const parseData = JSON.parse(savedFeetbackForm);
+
+    if(parseData) {
+        const { email ='', message ='' } = parseData;
+        formInput.value = email;
+        formMessage.value = message;
+    }
+} 
+
+function onBtnSubmit(event) {
+    event.preventDefault();
+
+if (!form.email.value || !form.message.value || form.email.value.trim() === '' || form.message.value.trim() === '') {
+    alert('Заповніть поля!')
+    return;
+}
+
+console.log(formData);
+
+event.currentTarget.reset();
+localStorage.removeItem(LOCALSTORAGE_KEY);
+}
