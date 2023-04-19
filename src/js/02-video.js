b0ef6c3debc23d8ef5cb0b5a2e5c57c7693d574a
Evgeniy-D-2022 <116091668+Evgeniy-D-2022@users.noreply.github.com>
2023-04-19 15:47:49 +0200

    update_02_video_js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const currentTime = "videoplayer-current-time";
+
+const setCurrentTime = function(data) {
+    // data is an object containing properties specific to that event
+    localStorage.setItem(currentTime, data.seconds)
+};
+
+player.on('timeupdate', throttle(setCurrentTime, 1000));
+
+player.setCurrentTime(localStorage.getItem(currentTime));
+
 
