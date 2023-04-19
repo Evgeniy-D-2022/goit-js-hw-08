cdac291678409bdd7d762dd5d272c1a791cc1aa5
Evgeniy-D-2022 <116091668+Evgeniy-D-2022@users.noreply.github.com>
2023-04-19 12:12:05 +0200

    add simplelightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,43 @@
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const itemImage = document.querySelector('.gallery');
+
+const markupGallery = galleryItems.map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+<a class="gallery__link" href= "${original}">
+  <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+  /></a></li>`
+}).join('');
+
+itemImage.insertAdjacentHTML('beforeend', markupGallery);
+
+itemImage.addEventListener('click', onModal);
+
+function onModal(evt) {
+    evt.preventDefault();
+
+    const currentImage = evt.target;
+
+    if (currentImage.nodeName !== 'IMG') {
+        return;
+    }
+    let instance = basicLightbox.create(`<img class="gallery__image"
+    src="${currentImage.dataset.source}"/>`)
+
+    instance.show();
+
+    // console.dir(currentImage.nodeName);
+}
+
+
+
